0aae1ac3d932b1ed2fd050acbebf8b6460cec5cd
sergiokano <sergiocano.design@gmail.com>
2023-01-07 21:00:41 +0100

    login with spotify button done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from "react";
-import Register from "./components/Register/Register";
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import Register from "./components/Register/Register";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
-import Header from "./components/Header/Header";
-import Profile from "./components/Profile/Profile";
-import PostDetail from "./components/Home/Posts/PostDetail/PostDetail";
-import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
-import Search from "./components/Search/Search";
-import Dashboard from "./components/Dashboard/Dashboard";
-const code = new URLSearchParams(window.location.search).get("code")
+import LoginSpotify from "./components/Login/LoginSpotify";
+// import Header from "./components/Header/Header";
+// import Profile from "./components/Profile/Profile";
+// import PostDetail from "./components/Home/Posts/PostDetail/PostDetail";
+// import Home from "./components/Home/Home";
+// import Footer from "./components/Footer/Footer";
+// import Search from "./components/Search/Search";
+// import Dashboard from "./components/Dashboard/Dashboard";
+const code = new URLSearchParams(window.location.search).get("code");
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <LoginSpotify />
+      {/* <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          code ? <Dashboard code={code} /> : <Login />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        code ? <Dashboard code={code} /> : <Login />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/post/:_id" element={<PostDetail />} />
           <Route path="/search/:postName" element={<Search/>} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
