2cad8cb560f674be59a1e968bb0feae9b9b22a98
sergiokano <sergiocano.design@gmail.com>
2023-01-08 12:31:08 +0100

    albums received aftear search

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useAuth from "../../features/auth/useAuth";
 import { Container, Form } from "react-bootstrap";
-import SpotifyWebApi from 'spotify-web-api-node'
+import SpotifyWebApi from "spotify-web-api-node";
+
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+});
 
 export default function Dashboard({ code }) {
-  const accesToken = useAuth(code);
-  const [search, setSearch] = useState("");
+  const accesToken = useAuth(code)
+  const [search, setSearch] = useState("")
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    if (!accesToken) return
+        spotifyApi.setAccessToken(accesToken);
+  }, [accesToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([])
+    if (!accesToken) return 
+
+    spotifyApi.searchTracks(search).then(res => {
+        console.log(res.body.tracks.items)
+    })
+
+  },[search, accesToken])
+
   return (
     <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
       <Form.Control
@@ -14,7 +36,7 @@ export default function Dashboard({ code }) {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <div className="flex-grow-1 my-2" style={{overflowY: "auto"}}>
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
         Songs
       </div>
       <div>Bottom</div>
