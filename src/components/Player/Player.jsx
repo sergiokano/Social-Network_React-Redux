bab56ae7d04473ebade481052cc49a0e97d7616c
sergiokano <sergiocano.design@gmail.com>
2023-01-08 17:03:19 +0100

    share button done

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import SpotifyPlayer from "react-spotify-web-playback"
+import React from "react";
+import { useEffect, useState } from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
+import ShareSongButton from "./ShareSongButton";
 
-const Player = ({accessToken, trackUri}) => {
-const [play, setPlay] = useState(false)
-useEffect(() => setPlay(true), [trackUri])
+const Player = ({ accessToken, trackUri }) => {
+  const [play, setPlay] = useState(false);
+  useEffect(() => setPlay(true), [trackUri]);
 
-    if (!accessToken) return null
+  if (!accessToken) return null;
   return (
-    <SpotifyPlayer 
-    token={accessToken}
-    showSaveIcon
-    callback={state => {
-        if (!state.isPlaying) setPlay(false)
-    }}
-    play={play}
-    uris={trackUri ? [trackUri] : []}
-    />
-  )
-}
+    <div>
+        {trackUri && <ShareSongButton trackUri={trackUri} />}
+      <SpotifyPlayer
+        token={accessToken}
+        showSaveIcon
+        callback={(state) => {
+          if (!state.isPlaying) setPlay(false);
+        }}
+        play={play}
+        uris={trackUri ? [trackUri] : []}
+      />
+    </div>
+  );
+};
 
-export default Player
+export default Player;
---
+++ src/components/Player/ShareSongButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./ShareSongButton.scss"
+import { Button } from 'antd';
+import Icon from '@ant-design/icons';
+
+const ShareSongButton = ({ trackUri }) => {
+    const handleClick = () => {
+      window.open(`https://open.spotify.com/track/${trackUri}`, '_blank');
+    };
+  
+    return (
+        <div className="d-flex justify-content-center mb-4">
+        <Button onClick={handleClick} className="btn-primary btn-lg">
+          <Icon type="share-alt" />
+          Compartir
+        </Button>
+      </div>
+    );
+  };
+
+  export default ShareSongButton;
+
+
