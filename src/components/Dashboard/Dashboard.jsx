1c62abdaa172ed8d43c5f52ba5e9dab357dc4d21
sergiokano <sergiocano.design@gmail.com>
2023-01-08 12:11:25 +0100

    search bar done

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,23 @@
-import React from "react"
-import useAuth from "../../features/auth/useAuth"
+import { useState } from "react";
+import useAuth from "../../features/auth/useAuth";
+import { Container, Form } from "react-bootstrap";
+import SpotifyWebApi from 'spotify-web-api-node'
 
-export default function Dashboard({code}) {
-    const accesToken = useAuth(code)
-    return <div>{code}</div>
+export default function Dashboard({ code }) {
+  const accesToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{overflowY: "auto"}}>
+        Songs
+      </div>
+      <div>Bottom</div>
+    </Container>
+  );
 }
--- src/features/auth/useAuth.js
+++ src/features/auth/useAuth.js
@@ -1,18 +1,46 @@
 import { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 
 export default function useAuth(code) {
   const [accessToken, setAccesToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
 
+  console.log(refreshToken);
+
   useEffect(() => {
-    axios.post('http://localhost:8080/music/login', {
+    axios
+      .post("http://localhost:8080/music/login", {
         code,
-    }).then(res => {
-        console.log(res.data)
-    }).catch(() => {
-        window.location = '/'
-    })
+      })
+      .then((res) => {
+        setAccesToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        // window.location = '/'
+      });
   }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:8080/music/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccesToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = '/'
+        });
+    }, (expiresIn - 60) * 1000);
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
 }
