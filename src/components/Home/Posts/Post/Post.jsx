20d0c8a3e4078e1daeb82da0208f363981289496
sergiokano <sergiocano.design@gmail.com>
2023-01-09 00:11:17 +0100

    Post css improved

--- src/components/Home/Posts/Post/Post.jsx
+++ src/components/Home/Posts/Post/Post.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import "./Post.scss"
+import "./Post.scss";
+import Card from "react-bootstrap/Card";
 
 const Post = () => {
   const { posts } = useSelector((state) => state.posts);
 
   const post = posts.map((post) => {
     return (
-      <div className="post" key={post._id}>
-        <Link to={"/post/" + post._id}> 
-        <p> {post.description}</p>
-        </Link>
-      </div>
+      <Card
+        style={{ margin: "20px 20px", textDecoration: "none" }}
+        key={post._id}
+      >
+        <Card.Body>
+          <Link
+            to={"/post/" + post._id}
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <Card.Title>{post.description}</Card.Title>
+          </Link>
+        </Card.Body>
+      </Card>
     );
   });
-  return <div>
-    {post}</div>;
+  return <div>{post}</div>;
 };
 
 export default Post;
