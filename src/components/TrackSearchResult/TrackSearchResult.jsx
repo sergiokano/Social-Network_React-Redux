fae858e179a29c206b0b0dfe0370ffdb9aaab400
sergiokano <sergiocano.design@gmail.com>
2023-01-08 15:57:14 +0100

    css changed, creating player

--- src/components/TrackSearchResult/TrackSearchResult.jsx
+++ src/components/TrackSearchResult/TrackSearchResult.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-export default function TrackSearchResult({ track }) {
-    if (!track) {
-      return <div>Error: track is not defined</div>;
-    }
-  
-    return (
-      <div className="d-flex m-2 align-items-center">
-        <div>{track.title}</div>
-      </div>
-    );
+export default function TrackSearchResult({ track, chooseTrack }) {
+  if (!track) {
+    return <div>Error: track is not defined</div>;
   }
+  function handlePlay(){
+    chooseTrack(track)
+  }
+  return (
+    <div
+      className="d-flex m-2 align-items-center"
+      style={{ cursor: "pointer" }}
+      onClick={handlePlay}
+    >
+      <img
+        src={track.albumUrl}
+        style={{ height: "64px", width: "64px", marginRight: "16px" }}
+      />
+      <div className="ml-3">
+        <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
+      </div>
+    </div>
+  );
+}
