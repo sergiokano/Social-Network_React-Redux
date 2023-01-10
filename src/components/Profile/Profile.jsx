54eb9cceacffb0113b220d4f89138c7c789f15e1
sergiokano <sergiocano.design@gmail.com>
2023-01-10 02:52:35 +0100

    profile view changed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,34 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserInfo } from "../../features/posts/postsSlice";
 import Post from "../Home/Posts/Post/Post";
+import "./Profile.scss";
 
 const Profile = () => {
   const { userInfo } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
   useEffect(() => {
-    console.log("USEFECT");   
+    console.log("USEFECT");
     dispatch(getUserInfo());
-  }, [])
+  }, []);
 
-  
-  
   console.log("USER INFO", userInfo);
   return (
     <>
-    <h1>Profile</h1>
-    <p>Name: {userInfo.name}</p>
-    <p>Email: {userInfo.email}</p>
-    <p>Born Date: {userInfo.date}/{userInfo.month}/{userInfo.year}</p>
-    <p>Gender: {userInfo.gender}</p>
-
-      {userInfo?.postIds &&
-        <Post posts={userInfo.postIds}/>
-      }
-      </>
+      <div className="infoProfile">
+        <h1>Profile</h1>
+        <p>Name: {userInfo?.name}</p>
+        <p>Email: {userInfo?.email}</p>
+        <p>
+          Born Date: {userInfo?.date}/{userInfo?.month}/{userInfo?.year}
+        </p>
+        <p>Gender: {userInfo?.email}</p>
+      </div>
+      <div className="commentsProfile">
+        <h2>{userInfo?.name}'s Posts</h2>
+        {userInfo?.postIds && <Post posts={userInfo.postIds} />}
+      </div>
+    </>
   );
 };
 
